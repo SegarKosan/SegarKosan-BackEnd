e62542ea26a738821b7d8a6eb25c74c6ee0bdbea
Rizal Kalam <rizalkalam78@gmail.com>
2025-12-03 18:54:40 +0700

    change db to mongo atlas

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // Pastikan fallback ke localhost jika env tidak ada
-    const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/segarkosan";
+    const uri =
+      process.env.MONGO_URI ||
+      "mongodb+srv://khoirurizal0507_db_user:863Fy0y5hTHbNRqt@cluster0.sjf84mg.mongodb.net/?appName=Cluster0";
 
     // Hapus opsi deprecated jika pakai Mongoose v6+
     const conn = await mongoose.connect(uri);
