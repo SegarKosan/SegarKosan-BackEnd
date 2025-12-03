26652e00de16b1259115d909504b7a3a8eb9e0aa
Rizal Kalam <rizalkalam78@gmail.com>
2025-12-03 19:28:26 +0700

    change config db to mongo atlas

--- src/config/db.js
+++ src/config/db.js
@@ -2,18 +2,20 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    // Pastikan fallback ke localhost jika env tidak ada
-    const uri =
-      process.env.MONGO_URI ||
-      "mongodb+srv://khoirurizal0507_db_user:863Fy0y5hTHbNRqt@cluster0.sjf84mg.mongodb.net/segarkosan?retryWrites=true&w=majority&appName=Cluster0";
+    const uri = process.env.MONGO_URI;
 
-    // Hapus opsi deprecated jika pakai Mongoose v6+
-    const conn = await mongoose.connect(uri);
+    if (!uri) {
+      throw new Error("MONGO_URI is missing!");
+    }
+
+    const conn = await mongoose.connect(uri, {
+      dbName: "segarkosan",
+    });
 
     console.log(`[DB] MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`[DB] Error: ${error.message}`);
-    process.exit(1); // Matikan proses jika DB gagal connect
+    process.exit(1);
   }
 };
 
