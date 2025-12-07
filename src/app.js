6290c8c59e30178de3c5dfc3958ca11f7b2289e0
Rizal Kalam <rizalkalam78@gmail.com>
2025-12-07 11:07:24 +0700

    fix cors

--- src/app.js
+++ src/app.js
@@ -11,29 +11,25 @@ const allowedOrigins = [
   "http://localhost:3000",
 ];
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // allow no-origin (Postman, curl, SSR Next.js)
-      if (!origin) return callback(null, true);
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    allowedOrigins.includes(req.headers.origin) ? req.headers.origin : "*"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("Not allowed by CORS: " + origin));
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
-
-// global OPTIONS handler
-app.options("*", cors());
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(express.json());
-
 app.use("/auth", require("./routes/auth.routes"));
 
 app.get("/test-db", async (req, res) => {
